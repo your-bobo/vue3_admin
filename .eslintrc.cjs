6a282395b214a5e5d353a48f7995a94da69903b7
bobo <1360601571@qq.com>
2022-11-29 15:28:28 +0800

    first commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,23 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
