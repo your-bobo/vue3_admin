f0e98bf1abbdd796f250f9c9ed93bf5cf6efd39f
bobo <1360601571@qq.com>
2022-12-09 01:43:40 +0800

    新增用户弹出框静态搭建

--- src/views/main/system/user/components/UserModel.vue
+++ src/views/main/system/user/components/UserModel.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="model">
-    <el-dialog v-model="showDialog" title="新建用户" width="30%" center>
+    <el-dialog v-model="showDialog" title="新建用户" width="25%" center> 
+      <div class="form">
+        <el-form
+          label-position="right"
+          label-width="80px"
+          :model="userForm"
+        >
+          <el-form-item label="用户名">
+            <el-input v-model="userForm.name" placeholder="请输入用户名"/>
+          </el-form-item>
+          <el-form-item label="真实姓名">
+            <el-input v-model="userForm.realname" placeholder="请输入真实姓名"/>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input v-model="userForm.password" placeholder="请输入密码"/>
+          </el-form-item>
+          <el-form-item label="电话号码">
+            <el-input v-model="userForm.phone" placeholder="请输入电话号码"/>
+          </el-form-item>
+          <el-form-item label="角色">
+            <el-select v-model="userForm.role" placeholder="请选择角色">
+              <el-option label="Zone one" value="1" />
+              <el-option label="Zone two" value="2" />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="部门">
+            <el-select v-model="userForm.department" placeholder="请选择部门">
+              <el-option label="Zone one" value="1" />
+              <el-option label="Zone two" value="2" />
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </div>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="showDialog = false">取消</el-button>
@@ -15,15 +47,28 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { reactive } from 'vue';
 
 const showDialog = ref(false)
 function cahangeShowDialog() {
   showDialog.value = true
 }
 
+const userForm = reactive({
+  name: '',
+  realname: '',
+  password: '',
+  phone: '',
+  role: '',
+  department: ''
+})
+
 defineExpose({ cahangeShowDialog })
 
 </script>
 
 <style lang="less" scoped>
+.el-select {
+  width: 100%;
+}
 </style>
